2862cd403dd4b34c7fe86db236aec35219e33316
Jacob <jbmenashi@gmail.com>
2019-08-16 16:05:27 -0400

    more MLR

--- multiple_linear_regression/mlr.R
+++ multiple_linear_regression/mlr.R
@@ -8,3 +8,9 @@ set.seed(123)
 split = sample.split(dataset$OPS, SplitRatio = 0.8)
 training_set = subset(dataset, split == TRUE)
 test_set = subset(dataset, split == FALSE)
+
+# MLR on the training set
+regressor = lm(formula = OPS ~ .,
+               data = training_set,
+               )
+#then run summary(regressor) to see the P-Values
